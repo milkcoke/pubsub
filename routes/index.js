f69d74533f36381017ea0db5bd3f7f5b63186847
milkcoke <mbh023@konkuk.ac.kr>
2020-11-26 01:06:21 +0900

    UPDATE typo

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(requset, response, next) {
-  response.render('index', { title: 'Session-Cookie Test Code & SSE', user: requset.user });
+router.get('/', function(request, response, next) {
+  response.render('index', { title: 'Session-Cookie Test Code & SSE', user: request.user });
 });
 
 module.exports = router;
